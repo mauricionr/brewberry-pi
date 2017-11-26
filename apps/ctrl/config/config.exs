81f5c3fa60d61b1df0446370be6f01f4cd2ec3f4
mauricionr <mauricioskateboard@gmail.com>
2017-11-26 20:24:45 -0200

    increase volume to 20

--- apps/ctrl/config/config.exs
+++ apps/ctrl/config/config.exs
@@ -3,4 +3,4 @@ use Mix.Config
 import_config "#{Mix.env}.exs"
 
 config :ctrl,
-  brew_house: [power: 1000, efficiency: 0.80, volume: 11, wait_time: 20]
+  brew_house: [power: 1000, efficiency: 0.80, volume: 20, wait_time: 20]
